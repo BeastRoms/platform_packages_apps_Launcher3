Descendant/packages_apps_Launcher3

Add Google search double tap option
Change-Id: Ia9a263b43db3f241a1c8e215af4a9589dc7b32bd

src/com/android/launcher3/Workspace.java
@@ -36,6 +36,7 @@ import android.app.WallpaperManager;
 import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -324,16 +325,16 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
     private void triggerGesture(MotionEvent event) {
         switch(mGestureMode) {
-            // Stock behavior
-            case 0:
+            case 0: // Stock
                 break;
-            // Sleep
-            case 1:
+            case 1: // Screen off
                 Utils.switchScreenOff(getContext());
                 break;
-            // Flashlight
-            case 2:
+            case 2: // Flashlight
                 Utils.toggleCameraFlash();
+                break;
+            case 3: // Google search
+                launchGoogleSearch(getContext());
                 break;
         }
     }
@@ -3535,6 +3536,20 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         @Override
         public void onAnimationEnd(Animator animation) {
             onEndStateTransition();
+        }
+    }
+
+    public void launchGoogleSearch(Context context) {
+        Intent launchIntent = new Intent(Intent.ACTION_VIEW);
+        launchIntent.setPackage("com.google.android.googlequicksearchbox");
+        launchIntent.setClassName("com.google.android.googlequicksearchbox",
+                "com.google.android.googlequicksearchbox.SearchActivity");
+        if (Utils.isPackageInstalled(context,
+                "com.google.android.googlequicksearchbox")) {
+            context.startActivity(launchIntent);
+        } else {
+            Toast.makeText(context, R.string.pref_homescreen_dt_gestures_google_toast,
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }

BeastRoms/platform_packages_apps_Launcher3

src/com/android/launcher3/Workspace.java
@@ -36,6 +36,7 @@ import android.app.WallpaperManager;
 import android.appwidget.AppWidgetHostView;
 import android.appwidget.AppWidgetProviderInfo;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -324,16 +325,16 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
 
     private void triggerGesture(MotionEvent event) {
         switch(mGestureMode) {
-            // Stock behavior
-            case 0:
+            case 0: // Stock
                 break;
-            // Sleep
-            case 1:
+            case 1: // Screen off
                 BeastUtils.switchScreenOff(getContext());
                 break;
-            // Flashlight
-            case 2:
+            case 2: // Flashlight
                 BeastUtils.toggleCameraFlash();
+                break;
+            case 3: // Google search
+                launchGoogleSearch(getContext());
                 break;
         }
     }
@@ -3535,6 +3536,20 @@ public class Workspace extends PagedView<WorkspacePageIndicator>
         @Override
         public void onAnimationEnd(Animator animation) {
             onEndStateTransition();
+        }
+    }
+
+    public void launchGoogleSearch(Context context) {
+        Intent launchIntent = new Intent(Intent.ACTION_VIEW);
+        launchIntent.setPackage("com.google.android.googlequicksearchbox");
+        launchIntent.setClassName("com.google.android.googlequicksearchbox",
+                "com.google.android.googlequicksearchbox.SearchActivity");
+        if (BeastUtils.isPackageInstalled(context,
+                "com.google.android.googlequicksearchbox")) {
+            context.startActivity(launchIntent);
+        } else {
+            Toast.makeText(context, R.string.pref_homescreen_dt_gestures_google_toast,
+                    Toast.LENGTH_SHORT).show();
         }
     }
 }